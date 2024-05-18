a6f65805a494510f26f3b9065735f0140753269b
Ankit-47 <omkathet218@gmail.com>
2024-05-18 18:18:34 +0545

    added bottom to enter command

--- gemini-clone/src/Component/Main/Main.jsx
+++ gemini-clone/src/Component/Main/Main.jsx
@@ -11,8 +11,8 @@ const Main= ()=> {
         <img src={assets.user_icon} alt=''></img>
       </div>
       <div className="main-container">
-        <div className="grid">
-            <p><span>Hello, Ak</span></p>
+        <div className="greet">
+            <p><span>Hello, Ak.</span></p>
             <p>How can I help you today?</p>
         </div>
         <div className="cards">
@@ -32,6 +32,17 @@ const Main= ()=> {
                 <p>Improve the readability of following code</p>
                 <img src={assets.code_icon} alt=''/>
             </div>
+        </div>
+        <div className="main-bottom">
+          <div className="search-box">
+            <input type='text' placeholder='Enter a prompt here'/>
+            <div>
+              <img src={assets.gallery_icon} alt='' />
+              <img src={assets.mic_icon} alt='' />
+              <img src={assets.send_icon} alt='' />
+            </div>
+          </div>
+          <p className='botttom-info'>Gemini may display inaccurate info, including about people, so double-check its responses.</p>
         </div>
       </div>
     </div>
