0289d42271ea22973ee3eed19ecc8ccc2681fc04
Ankit-47 <omkathet218@gmail.com>
2024-05-20 18:14:52 +0545

    recent questions can be seen in sidebar

--- gemini-clone/src/Component/Sidebar/Sidebar.jsx
+++ gemini-clone/src/Component/Sidebar/Sidebar.jsx
@@ -1,12 +1,14 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import './sidebar.css';
 import { assets } from '../../assets/assets';
+import { Context } from '../../context/Context';
 
 // AIzaSyC7A_NxA4W7Sx4DtwnlAThugYfhOFUaAy
 
 const Sidebar = () => {
 
   const [extended, setExtended] = useState(false)
+  const { onSent, prevPrompts, setRecentPrompt } = useContext(Context)
 
 
   return (
@@ -20,10 +22,15 @@ const Sidebar = () => {
         {extended ?
           <div className="recent">
             <p className="recent-title">Recent</p>
-            <div className="recent-entry">
-              <img src={assets.message_icon} alt='' />
-              <p>What is react ... </p>
-            </div>
+            {prevPrompts.map((item, index) => {
+              return (
+                <div className="recent-entry">
+                  <img src={assets.message_icon} alt='' />
+                  <p>{item.slice(0,18)}...</p>
+                </div>
+              )
+            })}
+
           </div>
           : null}
       </div>
